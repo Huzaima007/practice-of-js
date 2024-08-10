b4960c4622900cf9d7868a6ee4f072a5aa182c8b
Huzaima007 <huzaimasiddiq17@gmail.com>
2024-08-10 19:39:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var weight = 150;
+var weight1 = 150;
+var total = weight + weight1;
+alert(total);
+console.log(total);
+
+var whatletsover = 4 % 2;
+console.log(whatletsover);
+
+
+prompt("huzii","fill it");
+console.lop(prompt);
